9661f398f497565d01ed4ecf8fe80dff46b75d5c
Blake Herbert <Bwherb1@gmail.com>
2023-02-28 17:39:13 -0500

    initial commit - Menu class

---
+++ Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lumberjack_Typing_Adventure
+{
+	public class Menu
+	{
+		public string prompt;
+		public string[] options;
+
+		public void DisplayOptions()
+		{
+			WriteLine(prompt);
+			for (int i = 0; i < options.Length; i++)
+			{
+				string currentOption = options[i];
+				WriteLine(currentOption);
+			}
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Lumberjack_Typing_Adventure;
+using static System.Console;
+
+MainMenu();
+ConsoleKeyInfo selection = ReadKey();
+
+
+
+if (selection.Key == ConsoleKey.F)
+{
+	Clear();
+	WriteLine("Keep practicing!");
+	ReadKey();
+}
+else if (selection.Key == ConsoleKey.J)
+{
+	Clear();
+	WriteLine("\nHere's some information");
+	ReadKey();
+}
+else if (selection.Key == ConsoleKey.Spacebar)
+{
+	WriteLine("Goodbye!");
+	Environment.Exit(0);
+}
+
+
+
+
+
+void MainMenu()
+{
+	string[] mainMenuOptions = { "F. New Game", "J. Info", "Space . Exit" };
+	Menu MainMenu = new Menu();
+	MainMenu.options = mainMenuOptions;
+	MainMenu.prompt = "Lumberjack ";
+	MainMenu.DisplayOptions();
+}
